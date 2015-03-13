61c8ef0deeeb14d5757d1cd2078339aa7edceb61
unknown <mcimek@HQHYQZ1.SIIPOLSKA.PL>
2015-03-13 12:21:06 +0100

    Proba brancha czesc 3

---
+++ src/projekt2/ParametrizedTest.java
@@ -0,0 +1,45 @@
+package projekt2;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(org.junit.runners.Parameterized.class)
+public class ParametrizedTest {
+	
+	private int salary;
+	private int result;
+	
+	@Parameters
+	public static Collection salaries(){
+		return Arrays.asList(new Object[][]{
+				{500, 500},
+				{5000, 500},
+				{10000, 1000},
+				{15000, 1000},
+				{21321, 1000},
+				{11111, 1000}
+		});
+	}
+	
+	public ParametrizedTest(int salary, int result){
+		this.salary = salary;
+		this.result = result;
+		String nazwa = "dupatam";
+		System.out.println(nazwa);
+	}
+	
+	private void zlo(String nienawisc){
+		System.out.println(nienawisc);
+	}
+	
+	@Test
+	public void testIt(){
+		Assert.assertEquals(result, EmpBussinessLogic.calculateAppraisal(salary), 0.0);
+	}
+}
